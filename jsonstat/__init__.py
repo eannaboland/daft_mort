3bcc09c1f6bf053ec72754794919f9542ad8cf82
ahanway <42982333+ahanway@users.noreply.github.com>
2020-05-11 11:25:40 +0100

    Add_CSO_Property_Inflation_Pull

---
+++ jsonstat/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# This file is part of https://github.com/26fe/jsonstat.py
+# Copyright (C) 2016-2017 gf <gf@26fe.com>
+# See LICENSE file
+
+from jsonstat.version import __version__
+
+from jsonstat.exceptions import JsonStatException
+from jsonstat.exceptions import JsonStatMalformedJson
+
+from jsonstat.dimension import JsonStatDimension
+from jsonstat.value import JsonStatValue
+from jsonstat.dataset import JsonStatDataSet
+from jsonstat.collection import JsonStatCollection
+
+from jsonstat.downloader import Downloader
+from jsonstat.parse_functions import *
+
+import os
+_examples_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "examples"))
+
